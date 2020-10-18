1e2f68b2d566b9419603c684f5c569b07c2d796d
Indra Lukmana <indralukmana@live.com>
2020-10-18 17:29:25 +0700

    📦 NEW: Add register mutation on register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -8,18 +8,35 @@ import {
 import { Form, Formik } from 'formik';
 import { NextPage } from 'next';
 import * as React from 'react';
+import { useMutation } from 'urql';
 import InputField from '../components/InputField';
 import Wrapper from '../components/Wrapper';
 
 type RegisterProps = {};
 
+const REGISTER_MUTATION = `mutation Register($username: String!, $password: String!){
+  register(options: {username: $username, password: $password}){
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      createdAt
+      updatedAt
+      username
+    }
+  }
+}`;
+
 const RegisterPage: NextPage<RegisterProps> = ({}) => {
+  const [{}, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper>
       <Formik
         initialValues={{ username: '', password: '' }}
         onSubmit={(values) => {
-          console.log({ values });
+          return register(values);
         }}
       >
         {({ isSubmitting }) => (
