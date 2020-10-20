54184f1ae8419ed6be76b42fc51b3a42450673ea
Indra Lukmana <indralukmana@live.com>
2020-10-20 23:47:26 +0700

    📦 NEW: Add posts graphql query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import { withUrqlClient } from 'next-urql';
 import NavBar from '../components/NavBar';
+import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
-const IndexPage = () => (
-  <>
-    <NavBar />
-    <div>Hello chakra-ui</div>
-  </>
-);
+const IndexPage = () => {
+  const [{ data, fetching: postsFetching }] = usePostsQuery();
+
+  return (
+    <>
+      <NavBar />
+      <div>Hello chakra-ui</div>
+      <br />
+      {data
+        ? data.posts.map((post) => <p key={post.id}>{post.title}</p>)
+        : null}
+    </>
+  );
+};
 
 export default withUrqlClient(createUrqlClient, { ssr: false })(IndexPage);
